233c6bbff16cd8ddb8b3f68b5e47f564a3981b8b
Ciran GÜRBÜZ <ciran.gurbuz@gmail.com>
2023-11-27 16:32:04 +0100

    Added tests

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from website import app
+
+
+@pytest.fixture
+def app_():
+    """Configuring app for testing session."""
+    app.config.update({
+        "TESTING": True,
+    })
+    
+    yield app
+
+
+@pytest.fixture
+def client(app_):
+    """Simulating a test client."""
+    return app_.test_client()
---
+++ tests/unit_tests/test_urls.py
@@ -0,0 +1,22 @@
+import pytest
+
+class TestURLs:
+    def test_homepage(self, client):
+        """Testing rendering of the home page."""
+        response = client.get("/")
+        assert b"Welcome to my website !" in response.data
+    
+    def test_summary(self, client):
+        """Testing rendering of the summary page."""
+        response = client.get("/summary")
+        assert b"Coding Languages" in response.data
+    
+    def test_projects(self, client):
+        """Testing rendering of the projects page."""
+        response = client.get("/projects")
+        assert b"Links" in response.data
+    
+    def test_about_me(self, client):
+        """Testing rendering of the about me page."""
+        response = client.get("/about_me")
+        assert b"software developer" in response.data
